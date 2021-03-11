ebb934ce50879708ed91a70f350bf11177c915b2
Camille BERNADAS <camillebernadas@gmail.com>
2021-03-11 12:03:34 +0100

    :construction: adding creation article

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require('express');
 const PORT = process.env.PORT || 5000
 const routerArticles = require('./routes/articles')
 const routerAuthentification = require('./routes/authentication')
+const bodyParser = require('body-parser')
+const urlEncodedParser = bodyParser.urlencoded({ extended: false })
 const cors = require('cors')
 
 const app = express();
 
 app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true}))
 
 
 app.use('/articles/', routerArticles)
